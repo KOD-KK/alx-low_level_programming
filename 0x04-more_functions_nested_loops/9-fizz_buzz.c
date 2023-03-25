92b35a701aef96025dfa5498f9a4df9a005cbbbe
KOD-KK <d.korley123@gmail.com>
2023-03-24 21:43:40 -0700

    Update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
-} 
+}
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-} 
+}
 else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
@@ -27,7 +27,7 @@ printf(" FizzBuzz");
 else if (i == 1)
 {
 printf("%d", i);
-} 
+}
 else
 {
 printf(" %d", i);
