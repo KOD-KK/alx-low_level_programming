807d20277d42cd28dae3a57faaa0db2a9b797b84
KOD-KK <d.korley123@gmail.com>
2023-05-04 13:52:00 -0700

    up

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,17 +10,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int j = 0;
-	int counter = 0;
-	unsigned long int num;
-	unsigned long int new = n ^ m;
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-	for (j = 63; j >= 0; j--)
+	for (i = 63; i >= 0; i--)
 	{
-		num = new >> i;
-		if (num & 1)
-			counter++;
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
 	}
 
-	return (counter);
+	return (count);
 }
