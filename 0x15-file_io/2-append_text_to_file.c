e13bb7b834384090d45bcc5a0bf97f4d09a9551e
KOD-KK <d.korley123@gmail.com>
2023-05-11 12:44:39 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,18 +11,18 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+int n, z, l = 0;
 if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[len];)
-len++;
+for (l = 0; text_content[l];)
+l++;
 }
-o = open(filename, O_WRONLY | O_APPEND);
-w = write(o, text_content, len);
-if (o == -1 || w == -1)
+n = open(filename, O_WRONLY | O_APPEND);
+z = write(n, text_content, l);
+if (n == -1 || w == -1)
 return (-1);
-close(o);
+close(n);
 return (1);
 }
