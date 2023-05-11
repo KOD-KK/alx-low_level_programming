615a0923ddf95c53630fda4d69aa15716898bfad
KOD-KK <d.korley123@gmail.com>
2023-05-11 13:01:27 -0700

    uo

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,9 +30,9 @@ return (buffer);
 
 void close_file(int fd)
 {
-int d;
-d = close(fd);
-if (d == -1)
+int c;
+c = close(fd);
+if (c == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
@@ -51,9 +51,10 @@ exit(100);
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
  */
+
 int main(int argc, char *argv[])
 {
-int f, t, r, w;
+int from, to, r, w;
 char *buffer;
 if (argc != 3)
 {
@@ -61,32 +62,30 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 buffer = create_buffer(argv[2]);
-f = open(argv[1], O_RDONLY);
+from = open(argv[1], O_RDONLY);
 r = read(from, buffer, 1024);
-t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-do
-
-{
-if (f == -1 || r == -1)
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+do {
+if (from == -1 || r == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
-w = write(t, buffer, r);
-if (t == -1 || w == -1)
+w = write(to, buffer, r);
+if (to == -1 || w == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't write to %s\n", argv[2]);
 free(buffer);
 exit(99);
 }
-r = read(f, buffer, 1024);
-t = open(argv[2], O_WRONLY | O_APPEND);
+r = read(from, buffer, 1024);
+to = open(argv[2], O_WRONLY | O_APPEND);
 } while (r > 0);
 free(buffer);
-close_file(f);
-close_file(t);
+close_file(from);
+close_file(to);
 return (0);
 }
